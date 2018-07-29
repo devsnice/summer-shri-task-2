0e53450c7a1d081308158a9e8e8a1960cd15969b
Borisov Semen <devsnice@gmail.com>
2018-07-29 14:54:57 +0300

    devices widget: added pager

---
+++ src/features/dashboardDevices/devicesList.js
@@ -0,0 +1,63 @@
+class DevicesList {
+  constructor() {
+    this.devicesList = document.querySelector(".widget-devices-list");
+    this.deviceItemWidth;
+
+    this.pager = this.initPager();
+  }
+
+  getDeviceItemWidth(devicesItem) {
+    return devicesItem.getBoundingClientRect().width;
+  }
+
+  /**
+   * It inits list pager, when it needed,
+   * when items aren't fit to width of device,
+   * it will display pager, which will shift items to the view area
+   */
+  initPager() {
+    const devicesItems = this.devicesList.querySelectorAll(
+      ".widget-devices-item"
+    );
+
+    const countItems = devicesItems.length;
+
+    if (countItems) {
+      this.devicesListWidth = this.devicesList.getBoundingClientRect().width;
+      this.deviceItemWidth = this.getDeviceItemWidth(devicesItems[0]);
+
+      const itemFitsInListWidth =
+        this.deviceItemWidth * countItems < this.devicesListWidth;
+
+      if (!itemFitsInListWidth) {
+        const itemsToViewCount = Math.ceil(
+          countItems - Math.round(this.devicesListWidth / this.deviceItemWidth)
+        );
+
+        const devicesPager = new Pager({
+          selectorId: "#devices-list-pager",
+          countPages: itemsToViewCount,
+          nextPageCallback: () => {
+            this.shiftPage();
+          },
+          prevPageCallback: () => {
+            this.shiftPage();
+          }
+        });
+
+        return devicesPager;
+      }
+
+      return null;
+    }
+  }
+
+  shiftPage() {
+    const currentPage = this.pager.getCurrentPage();
+
+    this.devicesList.style.marginLeft = `-${this.deviceItemWidth *
+      currentPage}px`;
+  }
+}
+
+const devicesList = new DevicesList();
