69c5ab706321a0d0c94691d8f92899e3ebe5fa0d
Borisov Semen <devsnice@gmail.com>
2018-08-07 20:20:49 +0300

    added correct verticalSlider logic

--- src/features/dashboardMain/sliderVertical.js
+++ src/features/dashboardMain/sliderVertical.js
@@ -1,6 +1,7 @@
+const MOBILE_MAX_WIDTH = 768;
+
 export default class VerticalSlider {
   /**
-   *
    * @param {string} elementSelector
    */
   constructor(elementSelector) {
@@ -9,14 +10,10 @@ export default class VerticalSlider {
     }
 
     this.config = {
-      visibleSliderItems: 2,
-      shiftPx: 140
+      visibleSliderItems: 2
     };
 
-    this.currentVisibleElements = this.config.visibleSliderItems;
-    this.currentListShifting = 0;
-
-    this.slider = document.querySelector(elementSelector);
+    this.slider = document.querySelector(`${elementSelector}`);
 
     if (!this.slider) {
       throw new Error(
@@ -28,65 +25,37 @@ export default class VerticalSlider {
   }
 
   hasSupport() {
-    return window.innerWidth > 768;
+    return window.innerWidth > MOBILE_MAX_WIDTH;
+  }
+
+  getNextSlideHeight() {
+    return this.controlNextElem.getBoundingClientRect().height;
   }
 
   initSlider() {
-    this.controlPrevElem = this.slider.querySelector("#slider-vertical-prev");
     this.controlNextElem = this.slider.querySelector("#slider-vertical-next");
     this.listWrapper = this.slider.querySelector(".widget-actions-list");
     this.sliders = this.slider.querySelectorAll(".widget-actions-item");
 
     this.eventsAmount = this.sliders.length;
+    this.nextSlideHeight = this.getNextSlideHeight();
 
     if (this.eventsAmount > this.config.visibleSliderItems) {
-      this.controlNextElem.classList = "widget-actions__next visible";
+      this.controlNextElem.classList.add("widget-actions__next_visible");
     }
 
     this.initEventListeners();
   }
 
   initEventListeners() {
-    this.controlPrevElem.addEventListener("click", () => {
-      this.shiftToPreviousSlide();
-    });
-
-    this.controlNextElem.addEventListener("click", () => {
-      this.shiftToNextSlide();
-    });
-  }
-
-  shiftToPreviousSlide() {
-    this.currentVisibleElements -= 1;
-    this.currentListShifting += this.config.shiftPx;
-
-    this.updateControls();
-    this.shiftSliderToNewPosition();
-  }
-
-  shiftToNextSlide() {
-    this.currentVisibleElements += 1;
-    this.currentListShifting -= this.config.shiftPx;
-
-    this.updateControls();
-    this.shiftSliderToNewPosition();
-  }
-
-  updateControls() {
-    const hasNextSlides = this.eventsAmount > this.currentVisibleElements;
-    const hasPreviousSlides =
-      this.config.visibleSliderItems < this.currentVisibleElements;
-
-    this.controlNextElem.classList = hasNextSlides
-      ? "widget-actions__next visible"
-      : "widget-actions__next";
-
-    this.controlPrevElem.classList = hasPreviousSlides
-      ? "widget-actions__prev visible"
-      : "widget-actions__prev";
-  }
-
-  shiftSliderToNewPosition() {
-    this.listWrapper.style.marginTop = `${this.currentListShifting}px`;
+    this.slider
+      .querySelector(".widget-actions-list")
+      .addEventListener("scroll", e => {
+        if (e.target.scrollTop > this.nextSlideHeight) {
+          this.controlNextElem.classList.remove("widget-actions__next_visible");
+        } else {
+          this.controlNextElem.classList.add("widget-actions__next_visible");
+        }
+      });
   }
 }
